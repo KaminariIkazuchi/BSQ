6be8f0dc423961eae72d04ae63a1e20fce9fc6d9
Warren HYPOLITE <kaminari@hypolite.crans.org>
2017-12-17 17:08:02 +0100

    Adding BSQ
    --------------------
    
    Transformation of the char * into char **
    Displaying the map (char **)
    
    --------------------
    
    Not expected characters were displayed
    Segmentation fault after displaying the map

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 ** File description:
 ** Main for BSQ
 */
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -13,11 +14,17 @@
 
 int main(int ac, char **av)
 {
+	char *buf;
+	char **tab;
 
-        if (ac == 1) {
-                write(2, "No arguments\n", 14);
-                return (84);
-        }
-        else if (ac > 1)
-                cat(av);
+	if (ac == 1) {
+		write(2, "No arguments\n", 14);
+		return (84);
+	}
+	else if (ac > 1) {
+
+		buf = cat(av);
+		tab = create_tab(buf);
+		disp_tab(tab);
+	}
 }
